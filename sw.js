0e67f8902b64e8da80f8c0a61a7e13be017daaec
amantaev0990-code <amantaev.0990@gmail.com>
2026-09-06 23:48:46 +0500

    Refresh PWA cache for SOLI-N field update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='solin-live-v5-20260905-2';
-const CORE=['./','./index.html','./styles.css?v=20260905-2','./app.js?v=20260905-2','./data-manifest.json','./data-meta.json','./manifest.webmanifest'];
+const C='solin-live-v6-20260906-1';
+const CORE=['./','./index.html','./styles.css?v=20260905-2','./app.js?v=20260906-1','./data-manifest.json','./data-meta.json','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(C).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
